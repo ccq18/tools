4185497676d0bfda803da2949dacd59c9d7d9850
ccq18 <348578429@qq.com>
2017-10-23 09:55:10 +0800

    no message

--- resources/views/words/index.blade.php
+++ resources/views/words/index.blade.php
@@ -40,11 +40,11 @@
     <nav class="navbar navbar-default navbar-fixed-bottom">
         <div class="container">
             <div class="row center-block">
-                <div class="col-md-4 col-xs-4 col-md-offset-2 col-xs-offset-2 ">
+                <div class="col-md-4 col-xs-3 col-md-offset-1 col-xs-offset-2 ">
                     <a style="font-size: 3.5em" href="{{build_url('/words/index',['word_id'=>$last])}}" class="glyphicon glyphicon-arrow-left"
                        aria-hidden="true"></a>
                 </div>
-                <div class="col-md-4 col-xs-4 col-md-offset-2 col-xs-offset-2">
+                <div class="col-md-4 col-xs-3 col-md-offset-1 col-xs-offset-3">
                     <a style="font-size: 3.5em" href="{{build_url('/words/index',['word_id'=>$next])}}" class="glyphicon glyphicon-arrow-right "
                        aria-hidden="true"></a>
                 </div>
