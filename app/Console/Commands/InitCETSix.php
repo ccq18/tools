8d095e448abb480d84e0c7ad154873a27981264b
ccq18 <348578429@qq.com>
2017-11-06 14:37:31 +0800

    no message

--- app/Console/Commands/InitCETSix.php
+++ app/Console/Commands/InitCETSix.php
@@ -48,12 +48,14 @@ class InitCETSix extends Command
                 }
                 $w = Word::where('word', $match[1])->first();
                 if (!empty($w)) {
-                    $info = $w->toArray();
-                    unset($info['id']);
-                    unset($info['created_at']);
-                    unset($info['updated_at']);
-                    unset($info['book_id']);
-                    $word = new Word($info);
+                    $word = new Word();
+                    $word->word  = $w->word;
+                    $word->base_str  = $w->base_str;
+                    $word->translate  = $w->translate;
+                    $word->sent  = $w->sent;
+                    $word->type  = $w->type;
+                    $word->simple_trans  = $w->simple_trans;
+                    $word->example  = $w->example;
                 } else {
                     $word = new Word();
                     $word->type = 'simple';
@@ -65,8 +67,8 @@ class InitCETSix extends Command
                 $word->number = $k+1;
                 $word->book_id = 3;
                 $word->saveOrFail();
-                $this->info($word->word);
             } catch (\Exception $e) {
+                // throw  $e;
                 $this->info($e->getMessage());
             }
 
