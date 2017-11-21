7a5fd822fa2ce64709f34e1e74eea707dbf00927
ccq18 <348578429@qq.com>
2017-11-21 10:44:46 +0800

    添加账号

---
+++ app/Console/Commands/AddUser.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Model\User;
+use Illuminate\Console\Command;
+
+class AddUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:add-user {email}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $email = $this->argument('email');
+
+        resolve(\App\Repositories\UserRepository::class)->createSimple($email);
+
+    }
+}
